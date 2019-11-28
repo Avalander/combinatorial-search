b5c952bbdbd3900f3b2961a389dd098ddc2e3043
Avalander <darth.quesorico@gmail.com>
2019-11-28 09:09:49 +0100

    Clean up code.

--- src/main/scala/search/NoRepeats.scala
+++ src/main/scala/search/NoRepeats.scala
@@ -3,24 +3,32 @@ package search
 object NoRepeats {
   def combinations (chars: Seq[Char]): Int = {
     val indexedChars = chars zip (0 until chars.size)
-    val partials = for {
-      (char, index) <- indexedChars
-      rest = indexedChars filter (_._2 != index)
-    } yield findCombinations((char, index), rest)
-    partials.sum
+    findPartials(indexedChars)
+    // val partials = for {
+    //   (char, index) <- indexedChars
+    //   rest = indexedChars filter (_._2 != index)
+    // } yield findCombinations(char, rest)
+    // partials.sum
   }
 
   private def findCombinations(
-    current: (Char, Int),
+    current: Char,
     chars: Seq[(Char, Int)],
-    last: Char = ' ',
-    total: Int = 0
+    last: Char,
+    total: Int
   ): Int = (current, last, chars) match {
-    case ((a, _), b, _) if a == b => total
+    case (a, b, _) if a == b => total
     case (_, _, Nil)         => total + 1
-    case _                   => (for {
+    case _                   => findPartials(chars, current, total)
+    // case _                   => (for {
+    //   (char, index) <- chars
+    //   rest = chars filter(_._2 != index)
+    // } yield findCombinations(char, rest, current, total)).sum
+  }
+
+  private def findPartials (chars: Seq[(Char, Int)], last: Char = ' ', total: Int = 0): Int =
+    (for {
       (char, index) <- chars
       rest = chars filter(_._2 != index)
-    } yield findCombinations((char, index), rest, current._1, total)).sum
-  }
+    } yield findCombinations(char, rest, last, total)).sum
 }
